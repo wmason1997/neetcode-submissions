b929b1e8a1cedef031ee73a9980cdfaac827fa15
William Mason <98278654+wmason1997@users.noreply.github.com>
2026-04-30 21:11:55 -0400

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        res = [""]
+        
+        corr_dict = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        for digit in digits:
+            tmp = []
+            for curStr in res:
+                for c in corr_dict[digit]:
+                    tmp.append(curStr + c)
+            res = tmp
+        return res
