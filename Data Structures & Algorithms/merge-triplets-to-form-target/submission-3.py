c876d5e61bc5a7af5706fd4cbdec9e0959ba080f
William Mason <98278654+wmason1997@users.noreply.github.com>
2026-05-29 07:57:41 -0400

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+
+        
+        N = len(triplets)
+        A, B, C = target
+        if N == 1:
+            return triplets[0] == target
+
+        for i in range(N - 1):
+            for j in range(i + 1, N):
+                if target == [max(triplets[i][0], triplets[j][0]), 
+                              max(triplets[i][1], triplets[j][1]),
+                              max(triplets[i][2], triplets[j][2])]:                    
+                    return True
+                elif (max(triplets[i][0], triplets[j][0]) <= A and
+                     max(triplets[i][1], triplets[j][1]) <= B and
+                     max(triplets[i][2], triplets[j][2]) <= B):
+                    triplets[j] = [max(triplets[i][0], triplets[j][0]), 
+                                   max(triplets[i][1], triplets[j][1]),
+                                   max(triplets[i][2], triplets[j][2])]
+                else:
+                    continue
+        return False
