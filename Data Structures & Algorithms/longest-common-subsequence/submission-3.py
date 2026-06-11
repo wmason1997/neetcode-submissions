0cc6476c4827333844b53ca60cd3a087c2f0eda2
William Mason <98278654+wmason1997@users.noreply.github.com>
2026-06-10 21:12:26 -0400

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        @lru_cache(None)
+        def dfs(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if text1[i] == text2[j]:
+                return 1 + dfs(i + 1, j + 1)
+            return max(dfs(i + 1, j), dfs(i, j + 1))
+        
+        return dfs(0, 0)
