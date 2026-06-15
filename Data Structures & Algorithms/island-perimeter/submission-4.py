48940c697c293e800d72cddd3f0ceeaa45d50a0a
William Mason <98278654+wmason1997@users.noreply.github.com>
2026-06-15 07:55:20 -0400

    Add: island-perimeter - submission-4

---
+++ Data Structures & Algorithms/island-perimeter/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(i, j):
+            if i >= ROWS or j >= COLS or i < 0 or j < 0 or grid[i][j] == 0:
+                return 1
+            if (i,j) in visit:
+                return 0
+            visit.add((i, j))
+            perim = dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+            return perim
+        
+        # Go over cells and find the first cell in the island. Once called, question will be answered.
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j]:
+                    return dfs(i, j)
+
+        return dfs(0, 0)
